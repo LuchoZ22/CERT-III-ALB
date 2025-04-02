89a75a6fb047d32d0a8932634bcbafcba0be001c
LuisZapata <luiszapatam1@upb.edu>
2025-04-02 07:38:24 -0400

    Added changes

--- init.sh
+++ init.sh
@@ -8,7 +8,6 @@ TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-meta
 
 
 AMI_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/ami-id)
-
 INSTANCE_ID=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
 PUBLIC_IPV4=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 MAC=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/mac)
@@ -17,8 +16,10 @@ TYPE=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/
 
 git clone https://github.com/LuchoZ22/CERT-III-ALB.git
 cd CERT-III-ALB
+sudo rm -rf /var/www/html/*
 sudo cp index.html /var/www/html/
 
+sudo sed -i "s/_AMI_ID_/${AMI_ID}/g" /var/www/html/index.html
 sudo sed -i "s/_INSTANCE_ID_/${INSTANCE_ID}/g" /var/www/html/index.html
 sudo sed -i "s/_IPV4_/${PUBLIC_IPV4}/g" /var/www/html/index.html
 sudo sed -i "s/_MAC_/${MAC}/g" /var/www/html/index.html
